4b95a4302b3b2b9d05fb63e322e07fd4e3a78c3f
Jaapio <jaap@ijaap.nl>
2022-05-17 23:29:33 +0200

    Add WIP comment

--- packages/guides-restructured-text/src/RestructuredText/Parser/Productions/Table/AbstractTableBuilder.php
+++ packages/guides-restructured-text/src/RestructuredText/Parser/Productions/Table/AbstractTableBuilder.php
@@ -29,6 +29,19 @@ abstract class AbstractTableBuilder implements TableBuilder
             foreach ($row->getColumns() as $col) {
                 $lines = explode("\n", $col->getContent());
 
+                /*
+                 * This part is broken, we want to parse all elements as fragment.
+                 * - However in a case we get just a single paragraph node we want to have just the internal span.
+                 * - the current enviroment is not passed. This makes it impossible to use links inside a table?
+                 * - just lines are accepted right now, but according to spec all types of elements are allowed. including complex stuff like
+                 *   code and other tables.
+                 *
+                 * This should somehow work, but we need to pass the current environment, which is in the parser instance.
+                 * Maybe we should move the "isStart" for the lists into the environment so we are sure that it works as expected
+                 * not based on the line iterator. As that will be at start once we are in a fragement?
+                 *
+                 * $node = $parser->parseFragment($col->getContent());
+                 */
                 if ($lineChecker->isListLine($lines[0], false)) {
                     $node = $parser->parseFragment($col->getContent())->getNodes()[0];
                 } else {
