336ecfc1df599a9c4b8f4fcec78f0ea2c82b0e1c
Jaapio <jaap@ijaap.nl>
2023-03-14 21:33:02 +0100

    Fix document link rendering
    During a refactoring the document links were broken, the alternative
    text was ignored.

--- packages/guides/src/References/Resolver/DocResolver.php
+++ packages/guides/src/References/Resolver/DocResolver.php
@@ -32,7 +32,7 @@ final class DocResolver implements Resolver
         return $this->createResolvedReference(
             $node->getUrl(),
             $context,
-            $entry,
+            $node->getText($entry->getTitle()->toString()),
             [],
             $node->getAnchor()
         );
@@ -46,13 +46,13 @@ final class DocResolver implements Resolver
     private function createResolvedReference(
         string        $file,
         RenderContext $environment,
-        DocumentEntry $entry,
+        string $text,
         array         $attributes = [],
         ?string       $anchor = null
     ): ResolvedReference {
         return new ResolvedReference(
             $file,
-            $entry->getTitle()->toString(),
+            $text,
             $environment->relativeDocUrl($file, $anchor),
             $attributes
         );
