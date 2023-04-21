ea4411ec785f0b1c24e0b6962e26ff7e7f67ca1f
mabolek <mathias@lilio.com>
2023-04-21 10:57:25 +0200

    Trigger pre and post events when rendering

---
+++ packages/guides/src/Event/PreRenderDocument.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace phpDocumentor\Guides\Event;
+
+use phpDocumentor\Guides\Handlers\RenderDocumentCommand;
+use phpDocumentor\Guides\NodeRenderers\NodeRenderer;
+use phpDocumentor\Guides\Nodes\DocumentNode;
+
+final class PreRenderDocument
+{
+    /** @var NodeRenderer<DocumentNode> */
+    private NodeRenderer $renderer;
+
+    private RenderDocumentCommand $command;
+
+    /**
+     * @param NodeRenderer<DocumentNode> $renderer
+     * @param RenderDocumentCommand $command
+     */
+    public function __construct(NodeRenderer $renderer, RenderDocumentCommand $command)
+    {
+        $this->renderer = $renderer;
+        $this->command = $command;
+    }
+
+    /**
+     * @return NodeRenderer<DocumentNode>
+     */
+    public function getRenderer(): NodeRenderer
+    {
+        return $this->renderer;
+    }
+
+    public function getCommand(): RenderDocumentCommand
+    {
+        return $this->command;
+    }
+}
--- packages/guides/src/Handlers/RenderDocumentHandler.php
+++ packages/guides/src/Handlers/RenderDocumentHandler.php
@@ -4,18 +4,31 @@ declare(strict_types=1);
 
 namespace phpDocumentor\Guides\Handlers;
 
+use phpDocumentor\Guides\Event\PostRenderDocument;
+use phpDocumentor\Guides\Event\PreRenderDocument;
 use phpDocumentor\Guides\NodeRenderers\NodeRenderer;
 use phpDocumentor\Guides\Nodes\DocumentNode;
+use Psr\EventDispatcher\EventDispatcherInterface;
+
+use function assert;
 
 final class RenderDocumentHandler
 {
     /** @param NodeRenderer<DocumentNode> $renderer */
-    public function __construct(private readonly NodeRenderer $renderer)
+    public function __construct(
+        private readonly NodeRenderer $renderer,
+        private readonly EventDispatcherInterface $eventDispatcher
+    )
     {
     }
 
     public function handle(RenderDocumentCommand $command): void
     {
+        $preRenderDocumentEvent = $this->eventDispatcher->dispatch(
+            new PreRenderDocument($this->renderer, $command)
+        );
+        assert($preRenderDocumentEvent instanceof PreRenderDocument);
+
         $command->getContext()->getDestination()->put(
             $command->getFileDestination(),
             $this->renderer->render(
@@ -23,5 +36,10 @@ final class RenderDocumentHandler
                 $command->getContext(),
             ),
         );
+
+        $postRenderDocumentEvent = $this->eventDispatcher->dispatch(
+            new PostRenderDocument($this->renderer, $command)
+        );
+        assert($postRenderDocumentEvent instanceof PostRenderDocument);
     }
 }
