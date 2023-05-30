0bb72740ea4e4e09c18e77b8877046e1a5847960
Jaapio <jaap@ijaap.nl>
2023-05-30 21:14:33 +0200

    Add compiler pass for textroles
    Allow domain specification in text roles by tags.

---
+++ packages/guides-restructured-text/src/RestructuredText/DependencyInjection/Compiler/TextRolePass.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace phpDocumentor\Guides\RestructuredText\DependencyInjection\Compiler;
+
+use phpDocumentor\Guides\RestructuredText\TextRoles\DefaultTextRoleFactory;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class TextRolePass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        $textRoleFactory = $container->findDefinition(DefaultTextRoleFactory::class);
+        $domains = [];
+        $textRoles = [];
+
+        foreach ($container->findTaggedServiceIds('phpdoc.guides.parser.rst.text_role') as $id => $tags) {
+            foreach ($tags as $tag) {
+                if (isset($tag['domain'])) {
+                    $domains[$tag['domain']][] = new Reference($id);
+                    continue;
+                }
+
+                $textRoles[] = new Reference($id);
+            }
+        }
+
+        $textRoleFactory->setArgument('$textRoles', $textRoles);
+        $textRoleFactory->setArgument('$domains', $domains);
+    }
+}
