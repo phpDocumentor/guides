6dbb1f782ccf7f03d3f80f4acf93895a0ad4aecf
lina.wolf <lwolf@w-commerce.de>
2023-07-05 19:24:18 +0000

    Support titled and not titled admonitions
    In sphinx a general admonition can have a title where the title is the text after the directive declaration. Meanwhile in specialized admonitions such a the note or warning directive the text after the directive declaration, if set, is interpreted as the first paragraph of the content.

--- packages/guides-restructured-text/src/RestructuredText/Nodes/AdmonitionNode.php
+++ packages/guides-restructured-text/src/RestructuredText/Nodes/AdmonitionNode.php
@@ -14,13 +14,14 @@ declare(strict_types=1);
 namespace phpDocumentor\Guides\RestructuredText\Nodes;
 
 use phpDocumentor\Guides\Nodes\CompoundNode;
+use phpDocumentor\Guides\Nodes\InlineCompoundNode;
 use phpDocumentor\Guides\Nodes\Node;
 
 /** @extends CompoundNode<Node> */
 class AdmonitionNode extends CompoundNode
 {
-    /** {@inheritDoc} */
-    public function __construct(private readonly string $name, private readonly string $text, array $value)
+    /** @param Node[] $value */
+    public function __construct(private readonly string $name, private readonly InlineCompoundNode|null $title, private readonly string $text, array $value, private readonly bool $isTitled = false)
     {
         parent::__construct($value);
     }
@@ -30,8 +31,18 @@ class AdmonitionNode extends CompoundNode
         return $this->name;
     }
 
+    public function getTitle(): InlineCompoundNode|null
+    {
+        return $this->title;
+    }
+
     public function getText(): string
     {
         return $this->text;
+    }
+
+    public function isTitled(): bool
+    {
+        return $this->isTitled;
     }
 }
