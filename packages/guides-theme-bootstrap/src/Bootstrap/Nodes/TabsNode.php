55592320095646604873fd57357f7ce2b5f70c4a
lina.wolf <lwolf@w-commerce.de>
2023-08-26 18:58:04 +0000

    [FEATURE] Provide directive to display tabs in bootstrap theme

---
+++ packages/guides-theme-bootstrap/src/Bootstrap/Nodes/TabsNode.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace phpDocumentor\Guides\Bootstrap\Nodes;
+
+use phpDocumentor\Guides\Nodes\InlineCompoundNode;
+use phpDocumentor\Guides\RestructuredText\Nodes\GeneralDirectiveNode;
+
+final class TabsNode extends GeneralDirectiveNode
+{
+    /** @param TabNode[] $tabs */
+    public function __construct(
+        protected readonly string $name,
+        protected readonly string $plainContent,
+        protected readonly InlineCompoundNode $content,
+        private readonly string $key,
+        private array $tabs,
+    ) {
+        parent::__construct($name, $plainContent, $content, $tabs);
+    }
+
+    /** @return TabNode[] */
+    public function getTabs(): array
+    {
+        return $this->tabs;
+    }
+
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+}
