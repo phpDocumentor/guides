f749a4aeb4cdb3f338ac525ed45bd85b23a55b6a
lina.wolf <lwolf@w-commerce.de>
2023-09-10 17:41:56 +0200

    [BUGFIX] Set option defaults via guides.xml

--- packages/guides/src/Settings/ProjectSettings.php
+++ packages/guides/src/Settings/ProjectSettings.php
@@ -4,25 +4,21 @@ declare(strict_types=1);
 
 namespace phpDocumentor\Guides\Settings;
 
-use function is_array;
-use function is_string;
-
 class ProjectSettings
 {
     /** @var array<string, string> */
-    private array $inventories;
-    private string $title;
-    private string $version;
-    private string $theme;
-
-    /** @param array<string, string|array<string, string>> $settingsArray */
-    public function __construct(array $settingsArray)
-    {
-        $this->title = isset($settingsArray['title']) && is_string($settingsArray['title']) ? $settingsArray['title'] : '';
-        $this->version = isset($settingsArray['version']) && is_string($settingsArray['version']) ? $settingsArray['version'] : '';
-        $this->inventories = isset($settingsArray['inventories']) && is_array($settingsArray['inventories']) ? $settingsArray['inventories'] : [];
-        $this->theme = isset($settingsArray['theme']) && is_string($settingsArray['theme']) ? $settingsArray['theme'] : 'default';
-    }
+    private array $inventories = [];
+    private string $title = '';
+    private string $version = '';
+    private string $theme = 'default';
+    private string $input = 'docs';
+    private string $output = 'output';
+    private string $inputFormat = 'rst';
+    /** @var string[]  */
+    private array $outputFormats = ['html'];
+    private string $logPath = 'php://stder';
+    private bool $failOnError = false;
+    private bool $showProgressBar = true;
 
     public function getTitle(): string
     {
@@ -64,5 +60,77 @@ class ProjectSettings
     public function setTheme(string $theme): void
     {
         $this->theme = $theme;
+    }
+
+    public function getInput(): string
+    {
+        return $this->input;
+    }
+
+    public function setInput(string $input): void
+    {
+        $this->input = $input;
+    }
+
+    public function getOutput(): string
+    {
+        return $this->output;
+    }
+
+    public function setOutput(string $output): void
+    {
+        $this->output = $output;
+    }
+
+    public function getInputFormat(): string
+    {
+        return $this->inputFormat;
+    }
+
+    public function setInputFormat(string $inputFormat): void
+    {
+        $this->inputFormat = $inputFormat;
+    }
+
+    public function getLogPath(): string
+    {
+        return $this->logPath;
+    }
+
+    public function setLogPath(string $logPath): void
+    {
+        $this->logPath = $logPath;
+    }
+
+    public function isFailOnError(): bool
+    {
+        return $this->failOnError;
+    }
+
+    public function setFailOnError(bool $failOnError): void
+    {
+        $this->failOnError = $failOnError;
+    }
+
+    public function isShowProgressBar(): bool
+    {
+        return $this->showProgressBar;
+    }
+
+    public function setShowProgressBar(bool $showProgressBar): void
+    {
+        $this->showProgressBar = $showProgressBar;
+    }
+
+    /** @return string[] */
+    public function getOutputFormats(): array
+    {
+        return $this->outputFormats;
+    }
+
+    /** @param string[] $outputFormats */
+    public function setOutputFormats(array $outputFormats): void
+    {
+        $this->outputFormats = $outputFormats;
     }
 }
