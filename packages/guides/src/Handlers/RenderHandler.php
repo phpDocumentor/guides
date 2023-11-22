bf543a2626eff5d78be5a45fd3293ec28172b4ce
lina.wolf <lwolf@w-commerce.de>
2023-11-22 10:20:32 +0100

    [FEATURE] Introduce Post Render Process Event

--- packages/guides/src/Handlers/RenderHandler.php
+++ packages/guides/src/Handlers/RenderHandler.php
@@ -4,17 +4,27 @@ declare(strict_types=1);
 
 namespace phpDocumentor\Guides\Handlers;
 
+use phpDocumentor\Guides\Event\PostRenderProcess;
 use phpDocumentor\Guides\Renderer\TypeRendererFactory;
+use Psr\EventDispatcher\EventDispatcherInterface;
+
+use function assert;
 
 final class RenderHandler
 {
-    public function __construct(private readonly TypeRendererFactory $renderSetFactory)
-    {
+    public function __construct(
+        private readonly TypeRendererFactory $renderSetFactory,
+        private readonly EventDispatcherInterface $eventDispatcher,
+    ) {
     }
 
     public function handle(RenderCommand $command): void
     {
         $renderSet = $this->renderSetFactory->getRenderSet($command->getOutputFormat());
         $renderSet->render($command);
+        $postRenderProcessEvent = $this->eventDispatcher->dispatch(
+            new PostRenderProcess($command),
+        );
+        assert($postRenderProcessEvent instanceof PostRenderProcess);
     }
 }
