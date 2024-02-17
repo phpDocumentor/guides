e75d1d2fd956a4f2b9617eb7999ae68be4d3c769
lina.wolf <lwolf@w-commerce.de>
2024-02-17 12:38:38 +0100

    [FEATURE] Add Meta-information markup
    https://www.sphinx-doc.org/en/master/usage/restructuredtext/directives.html#meta-information-markup
    
    resolves https://github.com/phpDocumentor/guides/issues/847

---
+++ packages/guides-restructured-text/src/RestructuredText/Directives/SectionauthorDirective.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of phpDocumentor.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @link https://phpdoc.org
+ */
+
+namespace phpDocumentor\Guides\RestructuredText\Directives;
+
+use phpDocumentor\Guides\Nodes\AuthorNode;
+use phpDocumentor\Guides\Nodes\Node;
+use phpDocumentor\Guides\RestructuredText\Parser\BlockContext;
+use phpDocumentor\Guides\RestructuredText\Parser\Directive;
+use Psr\Log\LoggerInterface;
+
+use function preg_match;
+
+final class SectionauthorDirective extends BaseDirective
+{
+    public const NAME_EMAIL_REGEX = '/^(?P<name>[\w\s]+)(?: <(?P<email>[^>]+)>)?$/';
+
+    public function __construct(
+        private readonly LoggerInterface $logger,
+    ) {
+    }
+
+    public function getName(): string
+    {
+        return 'sectionauthor';
+    }
+
+    /**
+     * When the default domain contains a class directive, this directive will be shadowed. Therefore, Sphinx re-exports it as rst-class.
+     *
+     * See https://www.sphinx-doc.org/en/master/usage/restructuredtext/basics.html#rstclass
+     *
+     * @return string[]
+     */
+    public function getAliases(): array
+    {
+        return ['codeauthor'];
+    }
+
+    /** {@inheritDoc}
+     *
+     * @param Directive $directive
+     */
+    public function process(
+        BlockContext $blockContext,
+        Directive $directive,
+    ): Node|null {
+        $input = $directive->getData();
+        $directiveName = $directive->getName();
+        if ($input === '') {
+            $this->logger->warning('`.. ' . $directiveName . ' ::` directive could not be parsed: `' . $input . '`', $blockContext->getLoggerInformation());
+
+            return null;
+        }
+
+        if (!preg_match(self::NAME_EMAIL_REGEX, $input, $matches)) {
+            $this->logger->warning('Content of `.. ' . $directiveName . ':: name <email>` must specify a name and can also specify an email', $blockContext->getLoggerInformation());
+
+            return null;
+        }
+
+        $name = $matches['name'];
+        $email = $matches['email'] ?? null;
+        $context = match ($directiveName) {
+            'sectionauthor' => AuthorNode::CONTEXT_SECTION,
+            default => AuthorNode::CONTEXT_CODE,
+        };
+
+        return new AuthorNode($name, [], $context, $email);
+    }
+}
---
+++ tests/Integration/tests/directives/sectionauthor/input/index.rst
@@ -0,0 +1,19 @@
+==============
+Document Title
+==============
+
+Some Section
+============
+
+..  sectionauthor:: Lina Wolf <lina.wolf@typo3.org>
+
+Some other section
+==================
+
+..  sectionauthor:: Lina Wolf
+
+..  codeauthor:: Lina Wolf <lina.wolf@typo3.org>
+
+..  code-block:: rst
+
+    Lorem *Ipsum* Dolor
