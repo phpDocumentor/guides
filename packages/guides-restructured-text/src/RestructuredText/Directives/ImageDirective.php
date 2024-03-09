c34759bb7e1ec8551a0fb7d0c1891aa7b9c7f109
lina.wolf <lwolf@w-commerce.de>
2024-03-09 11:13:40 +0100

    [FEATURE] Support directive image with target
    resolves https://github.com/phpDocumentor/guides/issues/911
    
    (cherry picked from commit a3508e2135f00c2272b7498f7fcaea363900f8f6)

--- packages/guides-restructured-text/src/RestructuredText/Directives/ImageDirective.php
+++ packages/guides-restructured-text/src/RestructuredText/Directives/ImageDirective.php
@@ -14,12 +14,21 @@ declare(strict_types=1);
 namespace phpDocumentor\Guides\RestructuredText\Directives;
 
 use phpDocumentor\Guides\Nodes\ImageNode;
+use phpDocumentor\Guides\Nodes\Inline\DocReferenceNode;
+use phpDocumentor\Guides\Nodes\Inline\HyperLinkNode;
+use phpDocumentor\Guides\Nodes\Inline\LinkInlineNode;
+use phpDocumentor\Guides\Nodes\Inline\ReferenceNode;
 use phpDocumentor\Guides\Nodes\Node;
 use phpDocumentor\Guides\ReferenceResolvers\DocumentNameResolverInterface;
 use phpDocumentor\Guides\RestructuredText\Parser\BlockContext;
 use phpDocumentor\Guides\RestructuredText\Parser\Directive;
 
 use function dirname;
+use function filter_var;
+use function preg_match;
+
+use const FILTER_VALIDATE_EMAIL;
+use const FILTER_VALIDATE_URL;
 
 /**
  * Renders an image, example :
@@ -30,6 +39,12 @@ use function dirname;
  */
 final class ImageDirective extends BaseDirective
 {
+    /** @see https://regex101.com/r/9dUrzu/3 */
+    public const REFERENCE_REGEX = '/^([a-zA-Z0-9-_]+)_$/';
+
+    /** @see https://regex101.com/r/6vPoiA/2 */
+    public const REFERENCE_ESCAPED_REGEX = '/^`([^`]+)`_$/';
+
     public function __construct(
         private readonly DocumentNameResolverInterface $documentNameResolver,
     ) {
@@ -45,11 +60,38 @@ final class ImageDirective extends BaseDirective
         BlockContext $blockContext,
         Directive $directive,
     ): Node {
-        return new ImageNode(
+        $node = new ImageNode(
             $this->documentNameResolver->absoluteUrl(
                 dirname($blockContext->getDocumentParserContext()->getContext()->getCurrentAbsolutePath()),
                 $directive->getData(),
             ),
         );
+        if ($directive->hasOption('target')) {
+            $targetReference = (string) $directive->getOption('target')->getValue();
+            $node->setTarget($this->resolveLinkTarget($targetReference));
+        }
+
+        return $node;
+    }
+
+    private function resolveLinkTarget(string $targetReference): LinkInlineNode
+    {
+        if (filter_var($targetReference, FILTER_VALIDATE_EMAIL)) {
+            return new HyperLinkNode('', $targetReference);
+        }
+
+        if (filter_var($targetReference, FILTER_VALIDATE_URL)) {
+            return new HyperLinkNode('', $targetReference);
+        }
+
+        if (preg_match(self::REFERENCE_REGEX, $targetReference, $matches)) {
+            return new ReferenceNode($matches[1], '');
+        }
+
+        if (preg_match(self::REFERENCE_ESCAPED_REGEX, $targetReference, $matches)) {
+            return new ReferenceNode($matches[1], '');
+        }
+
+        return new DocReferenceNode($targetReference, '');
     }
 }
