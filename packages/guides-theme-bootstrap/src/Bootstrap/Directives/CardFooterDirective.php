ea70e321a87baa21871f65f4120230df72e7474d
lina.wolf <lwolf@w-commerce.de>
2024-04-21 09:27:21 +0200

    [BUGFIX] Display value of card-footer,
    Also support links in footer

--- packages/guides-theme-bootstrap/src/Bootstrap/Directives/CardFooterDirective.php
+++ packages/guides-theme-bootstrap/src/Bootstrap/Directives/CardFooterDirective.php
@@ -15,11 +15,15 @@ namespace phpDocumentor\Guides\Bootstrap\Directives;
 
 use phpDocumentor\Guides\Bootstrap\Nodes\Card\CardFooterNode;
 use phpDocumentor\Guides\Nodes\CollectionNode;
+use phpDocumentor\Guides\Nodes\Inline\AbstractLinkInlineNode;
 use phpDocumentor\Guides\Nodes\InlineCompoundNode;
 use phpDocumentor\Guides\Nodes\Node;
 use phpDocumentor\Guides\RestructuredText\Directives\SubDirective;
 use phpDocumentor\Guides\RestructuredText\Parser\BlockContext;
 use phpDocumentor\Guides\RestructuredText\Parser\Directive;
+
+use function array_merge;
+use function explode;
 
 class CardFooterDirective extends SubDirective
 {
@@ -33,10 +37,33 @@ class CardFooterDirective extends SubDirective
         CollectionNode $collectionNode,
         Directive $directive,
     ): Node|null {
+        $contentItems = [];
+        $buttonStyle = null;
+        if ($directive->hasOption('button-style')) {
+            $buttonStyle = $directive->getOption('button-style')->toString();
+            if ($buttonStyle === '') {
+                $buttonStyle = 'btn btn-primary';
+            }
+
+            $buttonStyle = explode(' ', $buttonStyle);
+        }
+
+        if ($directive->getDataNode() !== null) {
+            $content = $directive->getDataNode();
+            foreach ($content->getChildren() as $contentItem) {
+                if ($buttonStyle !== null && $contentItem instanceof AbstractLinkInlineNode) {
+                    $contentItem->setClasses(array_merge($contentItem->getClasses(), $buttonStyle));
+                }
+
+                $contentItems[] = $contentItem;
+            }
+        }
+
         return new CardFooterNode(
             $this->getName(),
             $directive->getData(),
-            $directive->getDataNode() ?? new InlineCompoundNode(),
+            new InlineCompoundNode($contentItems),
+            $collectionNode->getChildren(),
         );
     }
 }
