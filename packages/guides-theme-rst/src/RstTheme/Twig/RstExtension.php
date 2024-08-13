caa4439911b6cdd2d9c21a1f326cf08d349febad
lina.wolf <lwolf@w-commerce.de>
2024-08-13 12:28:15 +0200

    [FEATURE] Create a rst theme to convert markdown
    Convert basic mark-down into restructured Text
    
    * headlines
    * paragpraphs
    * links
    * lists

---
+++ packages/guides-theme-rst/src/RstTheme/Twig/RstExtension.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of phpDocumentor.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @link https://phpdoc.org
+ */
+
+namespace phpDocumentor\Guides\RstTheme\Twig;
+
+use phpDocumentor\Guides\NodeRenderers\NodeRenderer;
+use phpDocumentor\Guides\Nodes\Node;
+use phpDocumentor\Guides\Nodes\TitleNode;
+use phpDocumentor\Guides\RstTheme\Configuration\HeaderSyntax;
+use phpDocumentor\Guides\Twig\GlobalMenuExtension;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+use function min;
+use function str_repeat;
+use function strlen;
+
+final class RstExtension extends AbstractExtension
+{
+    /** @param NodeRenderer<Node> $nodeRenderer */
+    public function __construct(
+        private readonly NodeRenderer $nodeRenderer,
+    ) {
+        $this->menuExtension = new GlobalMenuExtension($this->nodeRenderer);
+    }
+
+    /** @return TwigFunction[] */
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('renderRstTitle', $this->renderRstTitle(...), ['is_safe' => ['rst'], 'needs_context' => false]),
+        ];
+    }
+
+    public function renderRstTitle(TitleNode $node, string $content): string
+    {
+        $headerSyntax = HeaderSyntax::from(min($node->getLevel(), 6));
+        $ret = '';
+        if ($headerSyntax->hasTopDelimiter()) {
+            $ret .= str_repeat($headerSyntax->delimiter(), strlen($content)) . "\n";
+        }
+
+        $ret .= $content . "\n" . str_repeat($headerSyntax->delimiter(), strlen($content));
+
+        return $ret;
+    }
+}
