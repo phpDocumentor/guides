d6130acbe42bc03e61ae37b343537281e007776a
lina.wolf <lwolf@w-commerce.de>
2024-10-11 19:46:45 +0200

    [FEATURE] Create a rst theme to convert markdown
    Convert basic mark-down into restructured Text
    
    * blockquote

--- packages/guides-theme-rst/src/RstTheme/Twig/RstExtension.php
+++ packages/guides-theme-rst/src/RstTheme/Twig/RstExtension.php
@@ -22,7 +22,12 @@ use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
 
+use function array_map;
+use function explode;
+use function implode;
 use function min;
+use function preg_replace;
+use function rtrim;
 use function str_repeat;
 use function strlen;
 
@@ -59,12 +64,14 @@ final class RstExtension extends AbstractExtension
         $content = implode("\n", $lines);
 
         $content = preg_replace('/(\n){2,}/', "\n\n", $content);
-        return rtrim($content)."\n";
+
+        return rtrim($content) . "\n";
     }
 
     public function renderRstIndent(string $text, int $indentNr): string
     {
-        $indent = str_repeat(' ', $indentNr*4);
+        $indent = str_repeat(' ', $indentNr * 4);
+
         return preg_replace('/^/m', $indent, $text);
     }
 
