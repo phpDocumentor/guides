b1200a199cbb32db288ae0f2c179d12f2a6ebee9
lina.wolf <lwolf@w-commerce.de>
2024-10-11 15:31:31 +0200

    [FEATURE] Support GitHub like Markdown Admonitions
    See https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax#alerts
    
    Resolves https://github.com/phpDocumentor/guides/issues/1089

--- packages/guides-restructured-text/src/RestructuredText/NodeRenderers/Html/AdmonitionNodeRenderer.php
+++ packages/guides-restructured-text/src/RestructuredText/NodeRenderers/Html/AdmonitionNodeRenderer.php
@@ -15,9 +15,9 @@ namespace phpDocumentor\Guides\RestructuredText\NodeRenderers\Html;
 
 use InvalidArgumentException;
 use phpDocumentor\Guides\NodeRenderers\NodeRenderer;
+use phpDocumentor\Guides\Nodes\AdmonitionNode;
 use phpDocumentor\Guides\Nodes\Node;
 use phpDocumentor\Guides\RenderContext;
-use phpDocumentor\Guides\RestructuredText\Nodes\AdmonitionNode;
 use phpDocumentor\Guides\TemplateRenderer;
 
 use function implode;
