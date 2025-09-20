0881d303fdc576f40433aad5765ecaf89a7a9e4f
Jaapio <jaap@ijaap.nl>
2025-09-20 21:33:59 +0200

    Improve buffer calls

---
+++ packages/guides-restructured-text/tests/benchmarks/BufferBench.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace phpDocumentor\Guides\RestructuredText;
+
+use PhpBench\Attributes\Iterations;
+use PhpBench\Attributes\Revs;
+use phpDocumentor\Guides\RestructuredText\Parser\Buffer;
+
+final class BufferBench
+{
+    private Buffer $buffer;
+
+    public function __construct()
+    {
+        $this->buffer = new Buffer();
+        $this->buffer->push('   This is a line with leading spaces.   ');
+        $this->buffer->push(' This is another line.');
+        $this->buffer->push(' Yet another line with spaces.   ');
+        $this->buffer->push(' Final line.');
+    }
+
+
+    #[Revs([1000, 10_000])]
+    #[Iterations(5)]
+    public function benchGetLines(): void
+    {
+        $this->buffer->getLines();
+    }
+}
